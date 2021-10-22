93f1c39a0cd352083e39a31dc56a94fe62aed239
mateuszbialowas <mateusz.bialowas2@gmail.com>
2021-10-22 21:44:13 +0200

    start zadanie2

---
+++ app/src/main/java/com/example/zadanie1/Question.java
@@ -0,0 +1,20 @@
+package com.example.zadanie1;
+import android.widget.Toast;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+
+    public Question(int questionId, boolean trueAnswer) {
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+
+
+}
