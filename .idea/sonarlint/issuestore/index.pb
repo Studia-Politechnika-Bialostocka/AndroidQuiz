1e508191faab1726b26e9788148940203e9c0c0b
mateuszbialowas <mateusz.bialowas2@gmail.com>
2021-10-23 13:24:24 +0200

    already save state

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,5 +3,3 @@
 app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
 P
  app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
-d
-4app/src/main/java/com/example/zadanie1/Question.java,0/f/0f7f7c9ee1d2458679ee4f691430d212b9a1c3ef
--- app/src/main/java/com/example/zadanie1/MainActivity.java
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -1,34 +1,45 @@
 package com.example.zadanie1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import lombok.SneakyThrows;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final String QUIZ_TAG = "MainActivity";
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
     private TextView questionTextView;
 
     private Question[] questions = new Question[] {
-            new Question(R.string.q_activity, true),
-            new Question(R.string.q_find_resources, false),
-            new Question(R.string.q_listener, true),
-            new Question(R.string.q_resources, true),
-            new Question(R.string.q_version, false),
+            new Question(R.string.q_1, true),
+            new Question(R.string.q_2, false),
+            new Question(R.string.q_3, true),
+            new Question(R.string.q_4, true),
+            new Question(R.string.q_5, false),
     };
-
-    private int currentIndex = 0;
-
+    private int currentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onCreate | currentIndex: " + currentIndex);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onCreate2 | currentIndex: " + currentIndex);
+
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
@@ -38,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         trueButton.setOnClickListener(new View.OnClickListener() {
+            @SneakyThrows
             @Override
             public void onClick(View v) {
                 checkAnswerCorrectness(true);
@@ -56,10 +68,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 currentIndex = (currentIndex + 1)%questions.length;
+                Log.d(QUIZ_TAG, "Wywołana została metoda nextButton | currentIndexNext: " + currentIndex);
+
                 setNextQuestion();
             }
         });
+        questionTextView.setText(questions[currentIndex].getQuestionId());
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onPause | currentIndex: " + currentIndex);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onStop | currentIndex: " + currentIndex);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onDestroy | currentIndex: "+ currentIndex);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda: onSaveInstanceState | " + "currentIndexNext: " + currentIndex);
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+
+    }
+
 
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
@@ -75,7 +133,5 @@ public class MainActivity extends AppCompatActivity {
     private void setNextQuestion() {
         questionTextView.setText(questions[currentIndex].getQuestionId());
     }
-
-
 
 }
